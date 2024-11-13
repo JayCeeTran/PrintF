a4bba5a26230d7d7e4f57ba4d3a6256e4bb56c9e
JayCeeTran <juha_tran@hotmail.com>
2024-11-13 21:55:25 +0200

    minorfixes

--- ft_printf.c
+++ ft_printf.c
@@ -16,35 +16,48 @@
 #include <stdint.h>
 void	check_symbol(const char *s, va_list args)
 {
-	switch (*s)
-	{
-	case 'c':
+	if (*s == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
-		break ;
-	case 's':
+	else if (*s == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
-		break ;
-	case 'p':
+	else if (*s == 'p')
 		ft_put_p(va_arg(args, uint64_t), "0123456789abcdef");
-		break ;
-	case 'd':
+	else if (*s == 'd')
 		ft_putnbr_fd(va_arg(args, int), 1);
-		break ;
-	case 'i':
+	else if (*s == 'i')
 		ft_putnbr_fd(va_arg(args, int), 1);
-		break ;
-	case 'u':
+	else if (*s == 'u')
 		ft_putnbr_unsig(va_arg(args, unsigned int), 1);
-		break;
-	case 'x':
+	else if (*s == 'x')
 		ft_put_hex_l(va_arg(args, unsigned int), "0123456789abcdef");
-		break;
-	case 'X':
+	else if (*s == 'X')
 		ft_put_hex_u(va_arg(args, unsigned int), "0123456789ABCDEF");
-		break;
-	case '%':
+	else if (*s == '%')
+	{	
+		check_symbol(s + 1, args);
 		write (1, "%", 1);
-		break;
+	}
+}
+void	check_white(const char *s)
+{
+	if (*s == 'n')
+		ft_putchar_fd('\n', 1);
+	else if (*s == 't')
+		ft_putchar_fd('\t', 1);
+	else if (*s == 'v')
+		ft_putchar_fd('\v', 1);
+	else if (*s == 'r')
+		ft_putchar_fd('\r', 1);
+	else if (*s == 'b')
+		ft_putchar_fd('\b', 1);
+	else if (*s == '"')
+		ft_putchar_fd('"', 1);
+	else if (*s == '\0')
+		ft_putchar_fd('\0', 1);
+	else if (*s == '?')
+		ft_putchar_fd('?', 1);
+	else if (*s == ''')
+		write (1, ''', 1);
 	}
 }
 
@@ -55,14 +68,17 @@ int	ft_printf(const char *s, ...)
 	va_start(args, s);
 	while (*s)
 	{
-		if (*s == '%')
+		if (*s == '%' || *s == '\')
 		{
 			s++;
-			check_symbol(s++, args);
-			if (!*s)
-				break ;
-		}
-		write (1, &*s++, 1);
+			if (*s - 1 == '%')
+				check_symbol(s, args);
+			else
+				check_white(s);
+		}	
+		else
+			write (1, &*s, 1);
+		s++;
 	}
 	va_end(args);
 	return (0);
